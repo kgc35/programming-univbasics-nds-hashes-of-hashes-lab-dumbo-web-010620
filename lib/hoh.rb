5f6e03a2c76517e271b196c1a6c8ab3613079577
Kent Chan <kntchn@gmail.com>
2020-01-02 19:28:36 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,7 +1,7 @@
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
-# 
+#
 # The tests will guide your construction.
 #
 PORTION_1 = {
@@ -13,18 +13,18 @@ PORTION_1 = {
      }
    }
  }
-# 
+#
  PORTION_2 = {
    label: "Order"
  }
- 
+
  PORTION_3 = {
    label: "Family",
    sub_category: {
      label: "Genus",
    }
  }
-# 
+#
  PORTION_4 = {
    label: "Species",
    sub_category: nil
