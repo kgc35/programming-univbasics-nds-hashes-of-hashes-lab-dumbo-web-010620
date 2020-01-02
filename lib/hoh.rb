74b65e7dd8003b87003eba9e3364daac96f9d1bf
Kent Chan <kntchn@gmail.com>
2020-01-02 19:38:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -12,23 +12,23 @@ PORTION_1 = {
        label: "Class"
      }
    }
- }
+ };
 #
  PORTION_2 = {
    label: "Order"
- }
+ };
 
  PORTION_3 = {
    label: "Family",
    sub_category: {
      label: "Genus",
    }
- }
+ };
 #
  PORTION_4 = {
    label: "Species",
    sub_category: nil
- }
+ };
 
 def naming_system
   # Remember:
