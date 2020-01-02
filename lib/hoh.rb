cd41460406a13d4329dbe9d6884ad08eed4d6cde
Kent Chan <kntchn@gmail.com>
2020-01-02 19:37:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,8 +43,8 @@ def naming_system
   # HoH all the way to Species!
 naming_system = PORTION_1;
 naming_system[:sub_category][:sub_category][:sub_category] = PORTION_2;
-naming_system[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3;
-naming_system[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4;
+naming_system[:sub_category][:sub_category][:sub_category][:sub_category] => PORTION_3;
+naming_system[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] => PORTION_4;
 
 naming_system
 
